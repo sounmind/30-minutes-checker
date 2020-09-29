2f30fa7c6a2bd7ff1e37f96b2c5458332f3d28ab
sounmind <sounmind777@gmail.com>
2020-09-30 01:44:22 +0900

    Tag가 삭제되면 None이 되는 상태에서 오늘 마무리

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Tag(models.Model):
     title = models.CharField(max_length=50, null=True, default="Default Tag")
-    # created_time = models.DateTimeField( auto_now_add=True, primary_key=True, default=timezone.now)
+    # 필드들이 여러 개가 생기고 참조된다면, 그것은 어떻게 다뤄지고 다룰 수 있는 걸까?
 
     def __str__(self):
         return self.title
@@ -24,7 +24,8 @@ class Action(models.Model):
     # on_delete=models.SET_DEFAULT 일 때, Tag 테이블에서 해당 Tag가 삭제되면, default 값인 No Tag가 생성되길 예상했는데, ValueError at /admin/home/tag/4/delete/ Field 'id' expected a number but got 'No tag'. 오류 발생한다.
     # on_delete=models.SET_DEFAULT = 0 일 때, Tag 테이블에서 Action에 있는 Tag를 삭제할 때 오류 발생, FOREIGN KEY constraint failed
     # 내 예측대로라면, Tag가 default 값으로 변경될 때, Tag의 primary key에 접근해서 변경하려는 것으로 간주되어 오류가 발생하는 듯 하다.
-    tag = models.ForeignKey(Tag, on_delete=models.SET_DEFAULT,
+    # 결론 관계형 데이터베이스를 더 배우자. 그리고 질문하자.
+    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL,
                             null=True, default="Deleted")
 
     def __str__(self):
