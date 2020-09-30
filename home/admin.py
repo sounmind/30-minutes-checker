5b9c125f311b2a56fbfe0913154092b218378ef3
sounmind <sounmind777@gmail.com>
2020-10-01 02:22:49 +0900

    모델 관계 완성 (행동, 저자, 댓글, 태그)

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Tag, Action
+from .models import Tag, Action, Comment, Author
 
 # Register your models here.
 
 admin.site.register(Tag)
 admin.site.register(Action)
-
+admin.site.register(Comment)
+admin.site.register(Author)
--- home/models.py
+++ home/models.py
@@ -1,32 +1,49 @@
 from django.db import models
 from django.utils import timezone
 
+
+class Author(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50, default="")
+
+    def __str__(self):
+        return self.name
+
 # 행동에 기록되는 Tag 모음
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50, null=True, default="Default Tag")
-    # 필드들이 여러 개가 생기고 참조된다면, 그것은 어떻게 다뤄지고 다룰 수 있는 걸까?
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=50, default="")
 
     def __str__(self):
         return self.title
 
 
 # 행동이 기록되는 테이블
-
-
 class Action(models.Model):
-    created_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=200)
-    memo = models.TextField()
-    # 어떻게 Tag가 여러 개 입력되도록 할 것인가?
-    # on_delete=models.SET_NULL 일 때, Tag 테이블에서 해당 Tag가 삭제되면 default 값이 아니라 'None'이라고 된다.
-    # on_delete=models.SET_DEFAULT 일 때, Tag 테이블에서 해당 Tag가 삭제되면, default 값인 No Tag가 생성되길 예상했는데, ValueError at /admin/home/tag/4/delete/ Field 'id' expected a number but got 'No tag'. 오류 발생한다.
-    # on_delete=models.SET_DEFAULT = 0 일 때, Tag 테이블에서 Action에 있는 Tag를 삭제할 때 오류 발생, FOREIGN KEY constraint failed
-    # 내 예측대로라면, Tag가 default 값으로 변경될 때, Tag의 primary key에 접근해서 변경하려는 것으로 간주되어 오류가 발생하는 듯 하다.
-    # 결론 관계형 데이터베이스를 더 배우자. 그리고 질문하자.
-    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL,
-                            null=True, default="Deleted")
+    memo = models.TextField(blank=True)
+    # Tag model이 위에 있기 때문에 문자열로 표현해야 한다.
+    tag = models.ManyToManyField('Tag', blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title} | Tag: {self.tag}"
+        return f"{self.title} | Tag: {self.tag.all()} | Author: {self.author}"
+
+
+class Comment(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    # author가 사라지면 모든 comment가 삭제됨
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # action이 사라지면 모든 comment가 삭제됨
+    action = models.ForeignKey(Action, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.content}"
